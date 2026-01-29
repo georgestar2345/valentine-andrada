31f863b65395e039fd7300fa541c90a9958fec62
georgestar2345 <georgemunteanu147@yahoo.com>
2026-01-29 18:44:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 // Elemente
-const noBtn = document.getElementById("no");
-const yesBtn = document.getElementById("yes");
-const container = document.querySelector(".container");
-const success = document.getElementById("success");
-const playBtn = document.getElementById("playMusic");
-const iframe = document.getElementById("music");
+var noBtn = document.getElementById("no");
+var yesBtn = document.getElementById("yes");
+var container = document.querySelector(".container");
+var success = document.getElementById("success");
+var playBtn = document.getElementById("playMusic");
+var iframe = document.getElementById("music");
 
 /* ===== Muzica ===== */
 playBtn.addEventListener("click", function () {
-  iframe.src =
-    "https://www.youtube.com/embed/E8i32NXMxnc?autoplay=1&playsinline=1&loop=1&playlist=E8i32NXMxnc";
+  iframe.src = "https://www.youtube.com/embed/E8i32NXMxnc?autoplay=1&playsinline=1&loop=1&playlist=E8i32NXMxnc";
   playBtn.style.display = "none";
 });
 
@@ -17,9 +16,9 @@ playBtn.addEventListener("click", function () {
 function createHeart() {
   var heart = document.createElement("div");
   heart.className = "heart";
-  heart.innerText = "❤️";
+  heart.innerHTML = "&hearts;";
   heart.style.left = Math.random() * 100 + "vw";
-  heart.style.animationDuration = 3 + Math.random() * 3 + "s";
+  heart.style.animationDuration = (3 + Math.random() * 3) + "s";
   document.body.appendChild(heart);
 
   setTimeout(function () {
